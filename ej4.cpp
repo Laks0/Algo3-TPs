57fedca65c593cd68017ac1420aee38a794fd146
laks0 <laksjoaquin@gmail.com>
2023-09-10 14:59:34 -0300

    EJ4 FUNCIONA CON PD. el juez da segfault

--- ej4.cpp
+++ ej4.cpp
@@ -5,89 +5,79 @@
 
 using namespace std;
 
-int ultimoCostoFijo(const vector<int>& puestos, const vector<int>& postas) {
+int ultimoCosto(const vector<int>& puestos, int a, int b) {
 	int c = 0;
 
-	int principio = 0;
-	if (postas.size() >= 2) {
-		principio = postas[postas.size() - 2];
+	int posA = -1e7;
+	if (a < puestos.size()) {
+		posA = puestos[a];
 	}
 
-	// Ubicaciones en coordenadas de los límites del cálculo
-	// La posta anterior
-	int a = -1e5;
-	if (postas.size() >= 2) {
-		a = puestos[principio];
+	int posB = b;
+	if (b < puestos.size()) {
+		posB = puestos[b];
 	}
 
-	// La última posta
-	int b = puestos[postas[postas.size() - 1]];
+	int inicio = a;
+	if (a == puestos.size()) {
+		inicio = 0;
+	}
 
-	for (int i = principio; i < postas[postas.size() - 1]; i++) {
-		c += min(abs(puestos[i] - a), abs(puestos[i] - b));
+	for (int i = inicio; i < min(b, (int) puestos.size()); i++) {
+		c += min(abs(puestos[i] - posA), abs(puestos[i] - posB));
 	}
 
 	return c;
 }
 
-int ponerPostas(const vector<int>& puestos, int i, int k, int dist, vector<vector<vector<int>>>& mejores, vector<int>& postas) {
+int mejorCosto(const vector<int>& puestos, int i, int k, int ult, vector<vector<vector<int>>>& mejores) {
 	if (k == 0 && i == puestos.size()) {
-		int costoFinal = 0;
-
-		// Coordenada de la última posta
-		int ultimaPosta = puestos[postas[postas.size() - 1]];
-
-		for (int x = postas[postas.size() - 1]; x < puestos.size(); x++) {
-			costoFinal += abs(puestos[x] - ultimaPosta);
-		}
-
-		return costoFinal;
+		return ultimoCosto(puestos, ult, 2e7);
 	}
 
-	if (i == puestos.size()) {
+	if (k > 0 && i == puestos.size()) {
 		return 1e7;
 	}
 
-	if (k == 0) {
-		int alFinal = ponerPostas(puestos, puestos.size(), k, dist + puestos.size() - i, mejores, postas);
-		mejores[i][k][dist] = alFinal;
+	if (ult >= 0 && mejores[i][k][ult] != -1) {
+		return mejores[i][k][ult];
 	}
 
-	if (mejores[i][k][dist] != -1) {
-		return mejores[i][k][dist];
+	int poniendo = 1e7;
+	if (k > 0) {
+		int costoFijo = ultimoCosto(puestos, ult, i);
+		poniendo = mejorCosto(puestos, i+1, k-1, i, mejores) + costoFijo;
 	}
 
-	postas.push_back(i);
-	int costoFijo = ultimoCostoFijo(puestos, postas);
-	int poniendo = ponerPostas(puestos, i+1, k-1, 0, mejores, postas) + costoFijo;
+	int sinPoner = mejorCosto(puestos, i+1, k, ult, mejores);
 
-	postas.pop_back();
-	int sinPoner = ponerPostas(puestos, i+1, k, dist + 1, mejores, postas);
-
-	mejores[i][k][dist] = min(poniendo, sinPoner);
-
-	return mejores[i][k][dist];
+	mejores[i][k][ult] = min(poniendo, sinPoner);
+	return mejores[i][k][ult];
 }
 
-vector<int> reconstruirSol(int k, const vector<vector<vector<int>>>& mejores, const vector<int> puestos) {
+vector<int> reconstruirSol(const vector<int>& puestos, int k, const vector<vector<vector<int>>>& mejores) {
 	vector<int> postas;
 
-	int dist = 0;
+	int ult = puestos.size();
 
 	for (int i = 0; i < puestos.size() - 1; i++) {
-		int poniendo = 1e5;
-		if (k > 0)
-			poniendo = mejores[i+1][k-1][0];
-		int sinPoner = mejores[i+1][k][dist+1];
+		int poniendo = 1e7;
+		if (k > 0) {
+			int costoFijo = ultimoCosto(puestos, ult, i);
+			poniendo = mejores[i+1][k-1][i] + costoFijo;
+		}
 
-		if (sinPoner != -1 && sinPoner > poniendo) {
-			dist++;
-		}
-		if (poniendo != -1 && poniendo <= sinPoner) {
-			k--;
-			dist = 0;
+		int sinPoner = mejores[i+1][k][ult];
+
+		if (poniendo <= sinPoner) {
 			postas.push_back(puestos[i]);
+			ult = i;
+			k--;
 		}
+	}
+
+	if (k==1) {
+		postas.push_back(puestos[puestos.size() - 1]);
 	}
 
 	return postas;
@@ -96,6 +86,7 @@ vector<int> reconstruirSol(int k, const vector<vector<vector<int>>>& mejores, co
 int main() {
 	int c;
 	cin >> c;
+
 	for (int i = 0; i < c; i++) {
 		int n, k;
 		cin >> n >> k;
@@ -105,18 +96,13 @@ int main() {
 			cin >> puestos[x];
 		}
 
-		vector<int> postas;
+		vector<vector<vector<int>>> mejores(n, vector<vector<int>>(k+1, vector<int>(n+1, -1)));
 
-		// Estructura de memorización
-		vector<vector<vector<int>>> mejores(n, vector<vector<int>>(k+1, vector<int>(n, -1)));
+		int res = mejorCosto(puestos, 0, k, n, mejores);
 
-		vector<int> mejorSol;
-		int res = ponerPostas(puestos, 0, k, 0, mejores, postas);
-
-		postas = reconstruirSol(k, mejores, puestos);
+		vector<int> postas = reconstruirSol(puestos, k, mejores);
 
 		cout << res << endl;
-
 		for (int p : postas) {
 			cout << p << endl;
 		}
