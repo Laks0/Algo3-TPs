4626024bc4183d30c0ce39458c6aa75e9444a189
laks0 <laksjoaquin@gmail.com>
2023-09-10 19:14:04 -0300

    Ej4 pasa el juez! Había problemas con los números enormes

--- ej4.cpp
+++ ej4.cpp
@@ -5,10 +5,10 @@
 
 using namespace std;
 
-int ultimoCosto(const vector<int>& puestos, int a, int b) {
-	int c = 0;
+long long ultimoCosto(const vector<int>& puestos, int a, int b) {
+	long long c = 0;
 
-	int posA = -1e7;
+	int posA = -2e9;
 	if (a < puestos.size()) {
 		posA = puestos[a];
 	}
@@ -30,38 +30,38 @@ int ultimoCosto(const vector<int>& puestos, int a, int b) {
 	return c;
 }
 
-int mejorCosto(const vector<int>& puestos, int i, int k, int ult, vector<vector<vector<int>>>& mejores) {
+long long mejorCosto(const vector<int>& puestos, int i, int k, int ult, vector<vector<vector<long long>>>& mejores) {
 	if (k == 0 && i == puestos.size()) {
-		return ultimoCosto(puestos, ult, 2e7);
+		return ultimoCosto(puestos, ult, 3e7);
 	}
 
 	if (k > 0 && i == puestos.size()) {
-		return 1e7;
+		return 2e9;
 	}
 
 	if (ult >= 0 && mejores[i][k][ult] != -1) {
 		return mejores[i][k][ult];
 	}
 
-	int poniendo = 1e7;
+	long long poniendo = 2e9;
 	if (k > 0) {
-		int costoFijo = ultimoCosto(puestos, ult, i);
+		long long costoFijo = ultimoCosto(puestos, ult, i);
 		poniendo = mejorCosto(puestos, i+1, k-1, i, mejores) + costoFijo;
 	}
 
-	int sinPoner = mejorCosto(puestos, i+1, k, ult, mejores);
+	long long sinPoner = mejorCosto(puestos, i+1, k, ult, mejores);
 
 	mejores[i][k][ult] = min(poniendo, sinPoner);
 	return mejores[i][k][ult];
 }
 
-vector<int> reconstruirSol(const vector<int>& puestos, int k, const vector<vector<vector<int>>>& mejores) {
+vector<int> reconstruirSol(const vector<int>& puestos, int k, const vector<vector<vector<long long>>>& mejores) {
 	vector<int> postas;
 
 	int ult = puestos.size();
 
 	for (int i = 0; i < puestos.size() - 1; i++) {
-		int poniendo = 1e7;
+		int poniendo = 2e9;
 		if (k > 0) {
 			int costoFijo = ultimoCosto(puestos, ult, i);
 			poniendo = mejores[i+1][k-1][i] + costoFijo;
@@ -96,15 +96,17 @@ int main() {
 			cin >> puestos[x];
 		}
 
-		vector<vector<vector<int>>> mejores(n, vector<vector<int>>(k+1, vector<int>(n+1, -1)));
+		vector<vector<vector<long long>>> mejores(n, vector<vector<long long>>(k+1, vector<long long>(n+1, -1)));
 
-		int res = mejorCosto(puestos, 0, k, n, mejores);
+		long long res = mejorCosto(puestos, 0, k, n, mejores);
 
 		vector<int> postas = reconstruirSol(puestos, k, mejores);
 
 		cout << res << endl;
-		for (int p : postas) {
-			cout << p << endl;
+		cout << postas[0];
+		for (int x = 1; x < postas.size(); x++) {
+			cout << " " << postas[x];
 		}
+		cout << endl;
 	}
 }
