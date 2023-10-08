fd08be3c31f9fd366bfb26495f3b9adbe59ed990
laks0 <laksjoaquin@gmail.com>
2023-10-08 13:06:24 -0300

    EJ2 PASA EL JUEZ

--- ej2.cpp
+++ ej2.cpp
@@ -9,41 +9,47 @@ using namespace std;
 vector<pair<int, int>> adj(pair<int, int> v, int n, int m) {
 	vector<pair<int, int>> res;
 
+	if (v.first < m-1)
+		res.push_back({v.first + 1, v.second});
 	if (v.first > 0)
 		res.push_back({v.first - 1, v.second});
-	if (v.first < n-1)
-		res.push_back({v.first + 1, v.second});
+	if (v.second < n-1)
+		res.push_back({v.first, v.second + 1});
 	if (v.second > 0)
 		res.push_back({v.first, v.second - 1});
-	if (v.second < m-1)
-		res.push_back({v.first, v.second + 1});
 
 	return res;
 }
 
-int caminoMasCorto(pair<int, int> a, pair<int, int> b, int t, const vector<vector<int>>& map) {
-	int n = map.size();
-	int m = map[0].size();
-
+int tiempoMinimo(pair<int, int> a, pair<int, int> b, int t, const vector<vector<int>>& map) {
 	int depth = 0;
+
+	int m = map.size();
+	int n = map[0].size();
+
+	vector<vector<bool>> visitado(m, vector<bool>(n, false));
+	visitado[a.first][a.second] = true;
+
 	list<pair<int, int>> front;
 	list<pair<int, int>> next;
-	vector<vector<bool>> visitado(n, vector<bool>(m, false));
+
 	front.push_back(a);
-	visitado[a.first][a.second] = true;
 
 	while (front.size() > 0) {
 		for (pair<int, int> v : front) {
 			if (v == b) return depth;
 
 			for (pair<int, int> w : adj(v, n, m)) {
-				if (visitado[w.first][w.second]) continue;
+				int wx = w.first;
+				int wy = w.second;
 
-				int tij = map[w.first][w.second];
-				if (t + depth >= tij && tij != 0) continue;
+				if (visitado[wx][wy]) continue;
+
+				int tij = map[wx][wy];
+				if (tij <= t + depth && tij != 0) continue;
 
 				next.push_back(w);
-				visitado[w.first][w.second] = true;
+				visitado[wx][wy] = true;
 			}
 		}
 
@@ -58,29 +64,39 @@ int caminoMasCorto(pair<int, int> a, pair<int, int> b, int t, const vector<vecto
 int main() {
 	int c;
 	cin >> c;
+
 	for (int i = 0; i < c; i++) {
 		int n, m;
 		cin >> n >> m;
+		// n es la cantidad de filas y m de columnas
 
 		vector<vector<int>> map(m, vector<int>(n));
 
-		for (int y = 0; y < n; y ++) {
+		for (int y = 0; y < n; y++) {
 			for (int x = 0; x < m; x++) {
 				cin >> map[x][y];
 			}
 		}
 
-		pair<int, int> a, b;
-		cin >> a.first >> a.second >> b.first >> b.second;
+		pair<int, int> a;
+		pair<int, int> b;
 
-		int ida = caminoMasCorto(a, b, 1, map);
+		cin >> a.second >> a.first >> b.second >> b.first;
+
+		int ida = tiempoMinimo(a, b, 1, map);
 
 		if (ida == -1) {
 			cout << "IMPOSIBLE" << endl;
 			continue;
 		}
 
-		int vuelta = caminoMasCorto(b, a, 1+ida, map);
-		cout << ida << " " << vuelta + ida << endl;
+		int vuelta = tiempoMinimo(b, a, 1+ida, map);
+
+		if (vuelta == -1) {
+			cout << "IMPOSIBLE" << endl;
+			continue;
+		}
+
+		cout << ida  << " " << ida + vuelta << endl;
 	}
 }
